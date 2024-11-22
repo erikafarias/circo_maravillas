bc12ba6ab46e6fb0437cb91c7b036ff36c2a0e73
Erika Farias <erika.aldana.farias@gmail.com>
2024-11-21 23:15:18 -0300

    Diseño y estructura pagina funciones

--- js/funciones.js
+++ js/funciones.js
@@ -51,7 +51,22 @@ const funciones = [
       "Córdoba 4/5",
     ],
     distendidas: ["Palermo 3/4", "Córdoba 24/4"],
-    mensaje: "Últimas funciones",
+    mensaje: "ÚLTIMAS FUNCIONES",
+  },
+];
+
+const mapas = [
+  {
+    ubicacion: "Palermo",
+    mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3285.7370213798326!2d-58.42876352350519!3d-34.560213255203244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5a57fefe1ad%3A0xadc229f0dd3725c7!2sGEBA!5e0!3m2!1ses-419!2sar!4v1732240224882!5m2!1ses-419!2sar",
+  },
+  {
+    ubicacion: "Mar del Plata",
+    mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7468.559639972063!2d-57.55064013544198!3d-38.090346414709174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9584e7372d66e183%3A0x56394e9c1f34d433!2sAv.%20de%20los%20Trabajadores%206230%2C%20B7600%20Mar%20del%20Plata%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1732240712812!5m2!1ses-419!2sar",
+  },
+  {
+    ubicacion: "Cordoba",
+    mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3403.6187027816673!2d-64.1850350736236!3d-31.452162497983043!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432a2e29369f6a9%3A0x353fedceb9a4a6ef!2zSGlww7Nkcm9tbyBDw7NyZG9iYQ!5e0!3m2!1ses-419!2sar!4v1732240777707!5m2!1ses-419!2sar",
   },
 ];
 
@@ -83,7 +98,30 @@ document.addEventListener("DOMContentLoaded", () => {
       }
 
       const funcionMensaje = container.querySelector(".funciones-mensaje");
-      funcionMensaje.textContent = funcion.mensaje;
+      const mensaje = document.createElement("p");
+      mensaje.textContent = funcion.mensaje;
+      funcionMensaje.appendChild(mensaje);
     }
+  });
+
+  // Selecciona el iframe del mapa y los botones
+  const iframe = document.querySelector(".mapa iframe");
+  const botones = document.querySelectorAll(".direccion");
+
+  botones.forEach((boton) => {
+    boton.addEventListener("click", () => {
+      const ubicacionSeleccionada = boton.getAttribute("ubicacion");
+
+      botones.forEach((btn) => btn.classList.remove("activa")); // lo elimino de la opcion seleccionada anterior
+      boton.classList.add("activa");
+
+      const mapaSeleccionado = mapas.find(
+        (mapa) => mapa.ubicacion === ubicacionSeleccionada
+      );
+
+      if (mapaSeleccionado) {
+        iframe.src = mapaSeleccionado.mapa;
+      }
+    });
   });
 });
