af96be32bfeeff86ec0a36c538d6da703695e812
Erika Farias <erika.aldana.farias@gmail.com>
2024-11-23 02:25:30 -0300

    Modal envio

--- js/contacto.js
+++ js/contacto.js
@@ -19,11 +19,22 @@ document.addEventListener("DOMContentLoaded", () => {
   );
 
   formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
     if (!formulario.checkValidity()) {
       event.preventDefault();
       formulario.querySelectorAll(":invalid").forEach((campo) => {
         campo.classList.add("invalid");
       });
+    } else {
+      const modal = document.getElementById("modal-envio");
+      modal.classList.remove("hidden");
+      setTimeout(() => {
+        modal.classList.add("hidden");
+      }, 3000);
+      document.getElementById("cerrar").addEventListener("click", () => {
+        document.getElementById("modal-envio").classList.add("hidden");
+      });
+      document.getElementById("formulario-contacto").reset();
     }
   });
 });
